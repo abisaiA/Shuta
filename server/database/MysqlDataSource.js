231a7274b48f7940e533e45c23a4308fc9b6ad20
marigito <linyulemarigito@gmail.com>
2018-12-29 16:06:02 +0300

    finalizing lecturer landing page

---
+++ server/database/MysqlDataSource.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var MysqlDataSource = /** @class */ (function () {
+    function MysqlDataSource() {
+    }
+    MysqlDataSource.prototype.getConnection = function () {
+    };
+    return MysqlDataSource;
+}());
+exports.MysqlDataSource = MysqlDataSource;
