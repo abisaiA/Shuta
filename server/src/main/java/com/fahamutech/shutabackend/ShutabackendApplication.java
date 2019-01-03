ef14205addeb0a88db028d6346e93e5326488a41
Joshua <mama27j@gmail.com>
2019-01-03 12:59:34 +0300

    refresh

---
+++ server/src/main/java/com/fahamutech/shutabackend/ShutabackendApplication.java
@@ -0,0 +1,14 @@
+package com.fahamutech.shutabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShutabackendApplication.class, args);
+    }
+
+}
+
